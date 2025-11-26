04bca6386a0913d570cf60033fcdfeb8f8b65bc7
mingeung <jmk2229@naver.com>
2025-11-27 08:58:08 +0900

    feat: IncorrectNoteMapper 작성

--- codeBoy_backend/src/main/java/com/codeboy/mvc/model/dao/IncorrectNoteDao.java
+++ codeBoy_backend/src/main/java/com/codeboy/mvc/model/dao/IncorrectNoteDao.java
@@ -2,6 +2,7 @@ package com.codeboy.mvc.model.dao;
 
 import java.util.List;
 
+import com.codeboy.common.ProblemType;
 import com.codeboy.mvc.model.dto.Problem;
 
 public interface IncorrectNoteDao {
@@ -10,10 +11,10 @@ public interface IncorrectNoteDao {
     public List<Problem> selectIncorrectProblems(long memberId);
 
     //유저가 자신의 오답노트 안에서 문제를 삭제
-    public void deleteIncorrectProblem(long memberId, long problemId);
+    public void deleteIncorrectProblem(long memberId, long incorrectNoteId);
 
     //오답노트에 문제 추가
-    public void insertIncorrectProblem(long memberId, long problemId);
+    public void insertIncorrectProblem(long memberId, long problemId, long userProblemId, ProblemType problemType);
 }
 
 
